36bdf8e62b28736b33e5ad4fe72c4136755b29f1
sathishkumar <sathishkumar281995@gmail.com>
2019-01-28 21:49:25 +0530

    py file reading

--- server/PyfileCompare.py
+++ server/PyfileCompare.py
@@ -1,21 +1,62 @@
+import json
 import sys
 import difflib
-oldFile=sys.argv[1]
-updatedFile=sys.argv[2]
+oldFileName=sys.argv[1]
+updatedFileName=sys.argv[2]
 flagValue=sys.argv[3]
+resultList=[]
+file2LineNo=0;
+file1LineNo=0;
+oldFile=[];
+updatedFile=[];
+with open(oldFileName,"r") as oldFileObject:
+	oldFile=oldFileObject.readlines();
+
+with open(updatedFileName,"r") as updatedFileObject:
+	updatedFile=updatedFileObject.readlines();
 
 diff=difflib.Differ()
 if("1" in flagValue):
-	oldFile=oldFile.lower();
-	updatedFile=updatedFile.lower();
-if("2" in flagValue):
-	oldFile=oldFile.replace(" ","");
-	updatedFile=updatedFile.replace(" ","");
+	oldTempList=[]
+	updatedTempList=[]
+	for line in oldFile:
+		oldTempList.append(line.lower());
+	for line in updatedFile:
+		updatedTempList.append(line.lower());
+	oldFile=oldTempList;
+	updatedFile=updatedTempList;
 
-oldFileList=oldFile.split("\n");
-updatedFileList=updatedFile.split("\n");
+if("2" in flagValue):
+	oldTempList=[]
+	updatedTempList=[]
+	for line in oldFile :
+		oldTempList.append(line.replace(" ",""));
+	for line in updatedFile :
+		updatedTempList.append(line.replace(" ",""));
+	oldFile=oldTempList;
+	updatedFile=updatedTempList;
+
 
 #result=diff.compare(oldFileList,updatedFileList);
-result=difflib.ndiff(oldFileList,updatedFileList);
+result=difflib.ndiff(oldFile,updatedFile);
 for line in result:
-	print(line);
+	# print(line);
+	if(line.startswith("+")):
+		resultList.append("+"+" "+str(file2LineNo));
+		file2LineNo+=1;
+	elif(line.startswith("-")):
+		resultList.append("-"+" "+str(file1LineNo));
+		file1LineNo+=1;
+	elif(line.startswith(" ")):
+		resultList.append("");
+		file1LineNo+=1;
+		file2LineNo+=1;
+	elif(line.startswith("?")):
+		lastValue=resultList.pop();
+		j=0
+		while(j<len(line)):
+			if("^" in line[j] or "-" in line[j] or "+" in line[j]):
+				lastValue+=" "+str(j-1);
+			j+=1;
+		resultList.append(lastValue);
+print(json.dumps(resultList));
--- server/file_compare.js
+++ server/file_compare.js
@@ -4,48 +4,60 @@ const fs = require('fs');
 const Event = require('events');
 let event = new Event();
 module.exports = (app) => {
-	
-    /** Compare the shell output and file */
-    let doFileCompare=(pythonOutput) => {
-      let file1LineNo=0;
-      let file2LineNo=0;
-      let resultList=[]
-      let dataList=pythonOutput.split("\n");
-      // console.log(dataList);
-	  for(var i=0;i<dataList.length;i++){
-		  line=dataList[i];
-          if(line.startsWith("+")){
-                resultList.push("+"+" "+file2LineNo);
-                file2LineNo+=1;
-          }else if(line.startsWith("-")){
-                resultList.push("-"+" "+file1LineNo);
-                file1LineNo+=1;
-          }else if(line.startsWith(" ")){
-                resultList.push("");
-                file1LineNo+=1;
-                file2LineNo+=1;
-          }else if(line.startsWith("?")){
-              let lastValue=resultList.pop();
-                for(var j=0;j<line.length;j++){
-                    if("^"===line[j]||"-"===line[j]||"+"===line[j])
-                    {
-                        lastValue+=" "+(j-1);
-                    }
-                }
-                resultList.push(lastValue)
-              console.log(dataList[i-1])
-              console.log(dataList[i-1].length)
-              console.log(line)
-              console.log(line.length)
-			  i++;
-		  }
+    
+    
+    /**UUID function togenerate unique Id for for file name */
+    let create_UUID = () => {
+        let time = new Date().getTime();
+        let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+          var r = (time + Math.random() * 16) % 6 | 0;
+          time = Math.floor(time / 16);
+          return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+        return uuid;
       }
-      //console.log("resultList",resultList)
-    if(resultList.length==0)
-      resultList.push("*#*#File Format Not Supported*#*#")
-    // console.log("resultList",resultList)
-    return resultList;
-    }
+  
+    // /** Compare the shell output and file */
+    // let doFileCompare=(pythonOutput) => {
+    //   let file1LineNo=0;
+    //   let file2LineNo=0;
+    //   let resultList=[]
+    //   let dataList=pythonOutput.split("\n");
+    //   // console.log(dataList);
+	//   for(var i=0;i<dataList.length;i++){
+	// 	  line=dataList[i];
+    //       if(line.startsWith("+")){
+    //             resultList.push("+"+" "+file2LineNo);
+    //             file2LineNo+=1;
+    //       }else if(line.startsWith("-")){
+    //             resultList.push("-"+" "+file1LineNo);
+    //             file1LineNo+=1;
+    //       }else if(line.startsWith(" ")){
+    //             resultList.push("");
+    //             file1LineNo+=1;
+    //             file2LineNo+=1;
+    //       }else if(line.startsWith("?")){
+    //           let lastValue=resultList.pop();
+    //             for(var j=0;j<line.length;j++){
+    //                 if("^"===line[j]||"-"===line[j]||"+"===line[j])
+    //                 {
+    //                     lastValue+=" "+(j-1);
+    //                 }
+    //             }
+    //             resultList.push(lastValue)
+    //           console.log(dataList[i-1])
+    //           console.log(dataList[i-1].length)
+    //           console.log(line)
+    //           console.log(line.length)
+	// 		  i++;
+	// 	  }
+    //   }
+    //   console.log("resultList",resultList)
+    // if(resultList.length==0)
+    //   resultList.push("*#*#File Format Not Supported*#*#")
+    // // console.log("resultList",resultList)
+    // return resultList;
+    // }
 	
     /**File Upload router part and shell execution */
     app.route("/uploadFile").post((request, res, next) => {
@@ -62,12 +74,26 @@ module.exports = (app) => {
         condition = request.body.compareCondition.toString().replace(new RegExp(',','g'),' ');
         
 		console.log("condition",condition)
-		
+		let filename = []
+        let extend="";
+        for (var i = 0; i < 2; i++) {
+          let file = request.files.file[i];
+          extend = path.extname(request.files.file[i].name.toString());
+          filename.push(`${create_UUID()}${extend}`);
+          console.log(filename[0])
+          file.mv(`${__dirname}/public/${filename[i]}`, err => {
+            if (err) {
+              console.log(err);
+              event.emit("sendErrorResponse", err);
+              throw err;
+            }
+          });
+        }
         let timeout = setTimeout(() => {
             event.emit('sendErrorResponse', "File Format not Supported", res);
         }, 10000);
 
-        process1 = childProcess.spawn("python", ["PyfileCompare.py",`${request.files.file[0].data.toString()}`,`${request.files.file[1].data.toString()}`,`${condition}`]);
+        process1 = childProcess.spawn("python", ["PyfileCompare.py",`${__dirname}/public/${filename[0]}`,`${__dirname}/public/${filename[1]}`,`${condition}`]);
         shellOutput=""
 
         process1.stdout.on("data", (data) => {
@@ -80,8 +106,8 @@ module.exports = (app) => {
 
         process1.on('close',()=>{
         clearTimeout(timeout);
-        console.log(shellOutput.toString())
-        res.send({ 'response': doFileCompare(shellOutput.toString()), error:false });
+        console.log(JSON.parse(shellOutput.toString()))
+        res.send({ 'response': JSON.parse(shellOutput.toString()), error:false });
         })
 
         process1.on("error", (data) => { console.log("process error", data) });
